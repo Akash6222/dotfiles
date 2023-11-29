b9f48384dfd68b7328bce7613c141f51d227356f
Akash6222 <akashdubey25274@gmail.com>
2023-11-30 01:13:05 +0530

    Update .config directories

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,10 +9,10 @@ require("akash.icons")
 -- require("akash.which-key")
 
 -- Enable autochdir
-vim.cmd([[set autochdir]])
+--vim.cmd([[set autochdir]])
 
 -- Enable autochdir
-vim.o.autochdir = true
+--vim.o.autochdir = true
 
 -- Bufferline 
 require("bufferline").setup{}
