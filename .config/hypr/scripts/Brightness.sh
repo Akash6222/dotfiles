6a45d1f6270ad630d53740486e596fed24c981a3
Akash6222 <akashdubey25274@gmail.com>
2023-12-02 02:23:54 +0530

    fixed some bug of brillo

--- .config/hypr/scripts/Brightness.sh
+++ .config/hypr/scripts/Brightness.sh
@@ -4,48 +4,48 @@ iDIR="$HOME/.config/hypr/mako/icons"
 
 # Get brightness
 get_backlight() {
-	echo $(brillo -G)
+    echo $(brillo -G | awk '{print int($1+0.5)}')
 }
 
 # Get icons
 get_icon() {
-	current=$(get_backlight | sed 's/%//')
-	if   [ "$current" -le "20" ]; then
-		icon="$iDIR/brightness-20.png"
-	elif [ "$current" -le "40" ]; then
-		icon="$iDIR/brightness-40.png"
-	elif [ "$current" -le "60" ]; then
-		icon="$iDIR/brightness-60.png"
-	elif [ "$current" -le "80" ]; then
-		icon="$iDIR/brightness-80.png"
-	else
-		icon="$iDIR/brightness-100.png"
-	fi
+    current=$(get_backlight)
+    if   [ "$current" -le "20" ]; then
+        icon="$iDIR/brightness-20.png"
+    elif [ "$current" -le "40" ]; then
+        icon="$iDIR/brightness-40.png"
+    elif [ "$current" -le "60" ]; then
+        icon="$iDIR/brightness-60.png"
+    elif [ "$current" -le "80" ]; then
+        icon="$iDIR/brightness-80.png"
+    else
+        icon="$iDIR/brightness-100.png"
+    fi
 }
 
 # Notify
 notify_user() {
-	notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "$icon" "Brightness : $current%"
+    notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "$icon" "Brightness : $current%"
 }
 
 # Change brightness
 change_backlight() {
-	brillo -q "$1" && get_icon && notify_user
+    brillo -q "$1" && get_icon && notify_user
 }
 
 # Execute accordingly
 case "$1" in
-	"--get")
-		get_backlight
-		;;
-	"--inc")
-		change_backlight "-A 5"
-		;;
-	"--dec")
-		change_backlight "-U 5"
-		;;
-	*)
-		get_backlight
-		;;
+    "--get")
+        get_backlight
+        ;;
+    "--inc")
+        change_backlight "-A 5"
+        ;;
+    "--dec")
+        change_backlight "-U 5"
+        ;;
+    *)
+        get_backlight
+        ;;
 esac
 
