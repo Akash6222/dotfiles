bc5ec208dfcbac11bd0faffecbf01720611d51b0
Akash6222 <akashdubey25274@gmail.com>
2023-12-01 22:32:25 +0530

    updated script

--- update_config.sh
+++ update_config.sh
@@ -16,18 +16,18 @@ done
 git status
 
 # Ask for permission to proceed
-read -p "Do you want to proceed with copying directories and pushing changes to GitHub? (y/n): " response
+#read -p "Do you want to proceed with copying directories and pushing changes to GitHub? (y/n): " response
 
-if [[ -z "$response" || "$response" =~ ^[Yy]$ ]]; then
-    # Add, commit, and push the changes to GitHub (assuming you are using git)
-    cd "$dotfiles_dir"
-    git add .config
-    git commit -m "Update .config directories"
-    git push origin main
+#if [[ -z "$response" || "$response" =~ ^[Yy]$ ]]; then
+#    # Add, commit, and push the changes to GitHub (assuming you are using git)
+#    cd "$dotfiles_dir"
+#   git add .config
+#    git commit -m "Update .config directories"
+#    git push origin main
 
-    echo "Directories copied from ~/.config to dotfiles/.config and changes pushed to GitHub."
-else
-    echo "Operation canceled. No changes have been made or pushed."
-fi
+#    echo "Directories copied from ~/.config to dotfiles/.config and changes pushed to GitHub."
+#else
+#    echo "Operation canceled. No changes have been made or pushed."
+#fi
 
 
