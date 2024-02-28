6a6439451f1f528ff83d417c4dae646c9883cc3a
Akash6222 <akashdubey25274@gmail.com>
2024-02-28 21:34:01 +0530

    [MINOR] Changes in batterynotify.sh

--- .config/hypr/batterynotify.sh
+++ .config/hypr/batterynotify.sh
@@ -1,57 +1,76 @@
 #!/bin/bash
 
-# Battery warning script
-
-# Flag to track if charging stopped notification has been sent
-charging_stopped_notified=0
-# Flag to track if charging notification has been sent
-charging_notification_sent=0
-
-while true; do
-    # Get the battery status (Charging or Discharging)
-    battery_status=$(cat /sys/class/power_supply/BAT0/status)
-
-    # Get the battery percentage
+# Function to send charging notification
+send_charging_notification() {
     battery_percentage=$(cat /sys/class/power_supply/BAT0/capacity)
+    notify-send "Charging" "Battery level is currently $battery_percentage%."
+}
 
-    # If the battery is discharging
-    if [ "$battery_status" == "Discharging" ]; then
-        # Reset the flags if the battery is discharging
-        charging_stopped_notified=0
-        charging_notification_sent=0
-
-        # Check battery level and trigger warnings if necessary
-        if [ "$battery_percentage" -eq 20 ]; then
+# Function to send warning notifications
+send_warning_notification() {
+    case "$1" in
+        20)
             notify-send "Warning" "Battery level is 20%"
-            sleep 120
-        elif [ "$battery_percentage" -eq 15 ]; then
+            warning_executed=1  # Set the warning executed flag
+            ;;
+        15)
             notify-send "Warning" "Battery level is 15%. Soon system will be locked."
-            sleep 120
-        elif [ "$battery_percentage" -lt 10 ]; then
+            warning_executed=1  # Set the warning executed flag
+            ;;
+        10)
             notify-send "Warning" "Battery level is below 10%. Locking screen in 10s."
-            sleep 10
             ~/.config/hypr/scripts/LockScreen.sh
+            warning_executed=1  # Set the warning executed flag
+            ;;
+    esac
+}
+
+# Function to monitor charging status
+monitor_charging_status() {
+    charging_stopped_notified=0
+    charging_status=""
+    warning_executed=0  # Flag to track whether the warning has been executed
+
+    while true; do
+        # Get the battery status (Charging or Discharging)
+        battery_status=$(cat /sys/class/power_supply/BAT0/status)
+
+        # If the battery is charging and charging notification hasn't been sent yet
+        if [ "$battery_status" == "Charging" ] && [ "$charging_status" != "Charging" ]; then
+            send_charging_notification
+            charging_status="Charging"
+            warning_executed=0  # Reset the warning executed flag
+        elif [ "$battery_status" != "Charging" ]; then
+            charging_status=""
         fi
 
-    # If the battery is not charging and charging stopped notification hasn't been sent yet
-    elif [ "$battery_status" == "Not charging" ] && [ "$charging_stopped_notified" -eq 0 ]; then
-        # Notify once when charging stops
-        notify-send "Charging Stopped" "Charging Stopped at Battery level $battery_percentage%."
-        
-        # Set the flag to indicate that charging stopped notification has been sent
-        charging_stopped_notified=1
-        charging_notification_sent=0
+        # If the battery is discharging
+        if [ "$battery_status" == "Discharging" ]; then
+            # Get the battery percentage
+            battery_percentage=$(cat /sys/class/power_supply/BAT0/capacity)
 
-    # If the battery is charging and charging notification hasn't been sent yet
-    elif [ "$battery_status" == "Charging" ] && [ "$charging_notification_sent" -eq 0 ]; then
-        # Send a charging notification
-        notify-send "Charging" "Battery level is currently $battery_percentage%."
-        
-        # Set the flag to indicate that charging notification has been sent
-        charging_stopped_notified=0
-        charging_notification_sent=1
-    fi
+            # Check battery level and trigger warnings if necessary
+            if [ "$battery_percentage" -eq 20 ] && [ "$warning_executed" -eq 0 ]; then
+                send_warning_notification 20
+            elif [ "$battery_percentage" -eq 15 ] && [ "$warning_executed" -eq 0 ]; then
+                send_warning_notification 15
+            elif [ "$battery_percentage" -eq 10 ] && [ "$warning_executed" -eq 0 ]; then
+                send_warning_notification 10
+            fi
+        fi
 
-    # Sleep for 1 minute before checking again
-    #sleep 60
-done
+        # If the battery is not charging and charging stopped notification hasn't been sent yet
+        if [ "$battery_status" == "Not charging" ] && [ "$charging_stopped_notified" -eq 0 ]; then
+            # Notify once when charging stops
+            battery_percentage=$(cat /sys/class/power_supply/BAT0/capacity)
+            notify-send "Charging Stopped" "Charging Stopped at Battery level $battery_percentage%."
+            charging_stopped_notified=1
+        fi
+
+        # Sleep for 1 minute before checking again
+        #sleep 60
+    done
+}
+
+# Start monitoring charging status in the background
+monitor_charging_status &
